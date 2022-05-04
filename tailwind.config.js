38d14316e39d85a9054aaa52a17ee66e38d0eb48
Hasan Al-Aref <hasan.alaref9@Hotmail.com>
2022-05-04 23:17:15 +0300

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./Project/**/*.{html,js}", "./index.html"],
   theme:   {
-    extend: {},
+    extend: {
+      colors: {
+        primary:   "#00bcd4",
+        secondary: {
+          100: "#f5f5f5",
+          200: "#eeeeee",
+        },
+      },
+
+      fontFamily: {
+        Montserrat: "Montserrat",
+      },
+    },
   },
   plugins: [],
 };
